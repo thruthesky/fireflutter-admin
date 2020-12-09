e264de68b50a99503b044bb722cdf5bb422e3fa2
YinG17 <simbulanja.17@gmail.com>
2020-12-09 22:03:17 +0800

    photos page updae

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -3,9 +3,15 @@
     <h1>This is an Photos page</h1>
   </div>
 
-  <div v-for="url in urls" :key="url" >
+  <h3>Image count: {{ imageCount }}</h3>
+  <div v-for="url in urls" :key="url" class="image-holder">
+    {{ url }}
     <img :src="url" alt="url" class="image">
+    <hr>
   </div>
+
+  <p v-if="loadingPhotos"> Loading photos ... </p>
+  <p v-if="nextPageToken == null"> No more photos ... </p>
 </template>
 
 <script lang="ts">
@@ -16,22 +22,62 @@ import 'firebase/storage'
 
 export default class Posts extends Vue {
   storageRef = firebase.storage().ref();
+  forumPhotos = this.storageRef.child('forum-photos');
 
+  /// if `null`, means the server returned all possible images.
+  /// can also be used to check if no more photos.
+  nextPageToken: any = '';
   urls: string[] = [];
+  imageCount = 0;
 
-  async created() {
-    const res = await this.storageRef.child('forum-photos').listAll();
+  loadingPhotos = false;
 
-    console.log('Forum photos count: ', res.items.length);
+  async fetchImages() {
+    if (this.loadingPhotos) return;
+
+    /// next page token is an empty string by default.
+    /// once we fetch more images and the result length is less than the maxResults
+    /// result.nextPageToken will contain a null value.
+    if (this.nextPageToken == null) return;
+    this.loadingPhotos = true;
+
+    const res = await this.forumPhotos.list({ maxResults: 20, pageToken: this.nextPageToken});
+
+    this.nextPageToken = res.nextPageToken;
+    console.log("next page token :", this.nextPageToken);
+
+    this.imageCount += res.items.length;
+    console.log("image count: ", this.imageCount);
+
     res.items.forEach(async item => {
         const downloadURL = await item.getDownloadURL();
         this.urls.push(downloadURL);
     })
+    this.loadingPhotos = false;
+  }
+
+  created() {
+    this.fetchImages();
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  
+  handleScroll(e: any) {
+    const bottomOfWindow =
+      document.documentElement.scrollTop + window.innerHeight >
+      document.documentElement.offsetHeight - 200;
+
+    if (bottomOfWindow) {
+      this.fetchImages();
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.image-holder {
+  margin-bottom: 1em;
+}
+
 .image {
   width: 100%;
 }
