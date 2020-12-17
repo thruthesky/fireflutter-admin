569a2f9af92cd00908535ef1b3c1160d21039555
JAEHO SONG <thruthesky@gmail.com>
2020-12-18 01:44:03 +0900

    domain customization

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <template>
   <section class="layout">
     <div class="header-wrapper">
-      <header
-        class="d-flex justify-content-between"
-        v-if="currentRoute != '/private-policy'"
-      >
-        <div class="d-flex">
-          <router-link class="logo" to="/">FireFlutter</router-link>
-          <router-link to="/login" v-if="app.notLoggedIn">Login</router-link>
-          <router-link to="/register" v-if="app.notLoggedIn"
-            >Register</router-link
-          >
-          <router-link to="/profile" v-if="app.loggedIn">Profile</router-link>
-          <router-link to="/contacts">Contacts</router-link>
-          <router-link to="/private-policy">Private Policy</router-link>
-          <router-link to="/about">About</router-link>
-          <router-link to="/logout" v-if="app.loggedIn">Logout</router-link>
-        </div>
-        <div>
-          <router-link to="/admin" v-if="app.isAdmin">Admin</router-link>
-        </div>
-      </header>
+      <Header />
     </div>
     <main>
       <router-view />
@@ -35,9 +16,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-import { Vue } from "vue-class-component";
+import { Vue, Options } from "vue-class-component";
 import router from "@/router";
+import Header from "@/components/Header.vue"; // @ is an alias to /src
 
+@Options({
+  components: {
+    Header
+  }
+})
 export default class RegisterForm extends Vue {
   app = new AppService();
   currentRoute = "";
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header
+    class="d-flex justify-content-between"
+    v-if="currentRoute != '/private-policy'"
+  >
+    <div class="d-flex">
+      <router-link class="logo" to="/">FireFlutter</router-link>
+      <router-link to="/login" v-if="app.notLoggedIn">Login</router-link>
+      <router-link to="/register" v-if="app.notLoggedIn">Register</router-link>
+      <router-link to="/profile" v-if="app.loggedIn">Profile</router-link>
+      <router-link to="/contacts">Contacts</router-link>
+      <router-link to="/private-policy">Private Policy</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/logout" v-if="app.loggedIn">Logout</router-link>
+    </div>
+    <div>
+      <router-link to="/admin" v-if="app.isAdmin">Admin</router-link>
+    </div>
+  </header>
+</template>
